5b90045e76da0871e8982d199a9e88df4b5b6f92
Giada Ehrlich <giada.ehrlich@epfl.ch>
2024-12-20 20:11:29 +0100

    modified final results with liam plots

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 import seaborn as sns
 from plotly.subplots import make_subplots
 import plotly.express as px
+from matplotlib import pyplot as plt
 
 # Generates the line plots for foreign percentage and domestic in function of the years
 def generate_year_plot(df):
@@ -21,6 +22,8 @@ def generate_year_plot(df):
             y=['Domestic Percentage', 'Foreign Percentage'], 
             title='Average Percentages Over Time',
             labels={'value': 'Percentage', 'variable': 'Percentages'}
+            width=1100,
+            height=500
     )
     fig.update_layout(title=dict(text="Impact of Budget on Foreign Percentage", x=0.5, xanchor='center'))
     fig.show()
@@ -61,7 +64,6 @@ def create_box_plot(df, column, y_axis_title, title, log=False):
             tickvals=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9],  
             ticktext=["10", "100", "1k", "10k", "100k", "1M", "10M ", "100M", "1B"],  
             title_text=f"Log Scale of {y_axis_title}" 
-        
         )
     return fig_box
 
@@ -100,6 +102,10 @@ def fig_combined_plots(fig_box, fig_bar, title_name, log=False):
         subplot_titles=["Class", ""],  # Empty title for second subplot
     )
 
+
+
+    
+
     # Add all traces from the box plot to the first row
     for trace in fig_box['data']:
         fig_combined.add_trace(trace, row=1, col=1)
@@ -109,20 +115,19 @@ def fig_combined_plots(fig_box, fig_bar, title_name, log=False):
         fig_combined.add_trace(trace, row=2, col=1)
 
     # Update layout to center the title and maintain consistency in axis titles
-    fig_combined.update_layout(
+        fig_combined.update_layout(
         title={
-            "text": f"Distribution of Profitability and {title_name} by Class",
-            "x": 0.5,
-            "xanchor": "center"
+                "text": f"Distribution of Profitability and {title_name} by Class",
+                "x": 0.5,
+                "xanchor": "center"
         },
         boxmode="group",  # Group the box plots side-by-side
-        yaxis_title=title_name,
-
         yaxis=dict(
-            title=title_name,
-            type="log" if log else None,  # Log scale for the first plot if `log` is True
+                title=title_name,
+                type="log" if log else None,  # Log scale for the first plot if `log` is True
+                tickvals=[1e-1, 1e0, 1e1, 1e2],  # Logarithmic scale tick values
+                ticktext=["-1", "0", "1", "10"],
         ),
-
 
 
         showlegend=True,
@@ -152,4 +157,38 @@ def fig_combined_plots(fig_box, fig_bar, title_name, log=False):
         ]
     )
     return fig_combined
+
+
+def plot_critics_score_distribution(data_domestic, data_foreign, bin_edges=None, output_path=None, save=False):
+    if bin_edges is None:
+        bin_edges = np.linspace(0, 1, 51)
+
+    plt.figure(figsize=(10, 6))
+
+    sns.histplot(data_domestic, 
+                 bins=bin_edges, 
+                 kde=True, 
+                 color='blue', 
+                 label='Movies with domestic % > 50%', 
+                 alpha=0.5)
+
+    sns.histplot(data_foreign, 
+                 bins=bin_edges, 
+                 kde=True, 
+                 color='red', 
+                 label='Movies with foreign % > 50%', 
+                 alpha=0.5)
+
+    plt.title('Distribution of Critics Score with KDE', fontsize=16)
+    plt.xlabel('Critics Score', fontsize=14)
+    plt.ylabel('Movie Count', fontsize=14)
+    plt.legend(fontsize=12)
+    plt.grid(axis='y', alpha=0.75)
+
+    plt.tight_layout()
+
+    if save:
+        plt.savefig(output_path, format='png', dpi=300)
+
+    plt.show()
 
